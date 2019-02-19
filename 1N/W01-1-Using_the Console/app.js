044926d440452ac0206b3ab5115d098f42b5a3e5
sunctsun <43174141+sunctsun@users.noreply.github.com>
2019-02-19 20:43:31 +0800

    Add files via upload

---
+++ 1N/W01-1-Using_the Console/app.js
@@ -0,0 +1,30 @@
+console.log('hello world');
+console.log(207410589); //id
+console.log(true);
+
+var greeting ='leexinting';
+console.log(greeting);
+console.log([2019,2,19]);  //today date
+console.log({firstName:'xinting',lastName:'lee'}); //object
+console.table({firstName:'xinting',lastName:'lee'});
+
+console.error('this is some error');
+console.warn('this is some warning');
+
+console.time();
+    console.log('hello world');
+    console.log('hello world');
+    console.log('hello world');
+    console.log('hello world');
+    console.log('hello world');
+
+console.timeEnd();
+
+//console.clear();
+
+/*
+  hello
+  every 
+  one
+
+*/
---
+++ 1N/W01-2-Variable-var-let-const/app.js
@@ -0,0 +1,53 @@
+//var,let,const
+
+var name ="leexinting";
+console.log(name);
+
+name = '李昕庭';
+console.log(name);
+
+name ="2019";
+console.log(name);
+
+var greeting;
+console.log(greeting);
+greeting ='Hello';
+console.log(greeting);
+
+//letter,numbers, _, $,
+//annot start with number
+
+//Multi word vars
+var firstName ="xinting"; //camel case
+var first_name ="Sunny"; //underscore
+var FirstName ="Dora";  //Pascal case
+var firstname;
+
+//let
+let name2 = "xinting lee";
+console.log(name2);
+name2 = '李昕庭';
+console.log(name2);
+name2 ="2019";
+console.log(name2);
+
+//const
+//const name3 ="xinting lee";
+//console.log(name3);
+//name3 = '李昕庭';
+//console.log(name3);
+//name3 = "2019";
+//console.log(name3);
+
+const person = {
+    name: 'xtl',
+    age:18
+};
+
+person.name = 'xtlee';
+person.age =18;
+console.log(person);
+
+const numbers =[1,2,3,4,5];
+numbers.push(6);
+console.log(numbers);
