b1c49e77af92c21bfb3fe56996b6bbcbfcf9f692
sunctsun <43174141+sunctsun@users.noreply.github.com>
2019-02-19 21:47:07 +0800

    Add files via upload

---
+++ 1N/W01-4-Math/app.js
@@ -0,0 +1,52 @@
+const num1 =100;
+const num2 =50;
+let res
+
+res =num1 + num2;
+console.log(res);
+
+res =num1 - num2;
+console.log(res);
+
+res =num1 * num2;
+console.log(res);
+
+res =num1 / num2;
+console.log(res);
+
+res =num1 % num2;
+console.log(res);
+
+res =Math.PI;
+console.log(res);
+
+res =Math.E;
+console.log(res);
+
+res =Math.round(2.4);
+console.log(res);
+
+res =Math.ceil(2.4);
+console.log(res);
+
+res =Math.floor(2.4);
+console.log(res);
+
+res =Math.abs(-7);
+console.log(res);
+
+res =Math.sqrt(25);
+console.log(res);
+
+res =Math.min(8,5,45,68,25);
+console.log(res);
+
+res =Math.max(12,57,48,69,3);
+console.log(res);
+
+res =Math.floor(Math.random()*10+1);
+console.log(res);
+
+res =Math.ceil(Math.random()*10);
+console.log(res);
+
---
+++ 1N/W01-5-String Method/app.js
@@ -0,0 +1,10 @@
+const firstName ='Will';
+const lastName ='Dell';
+
+let res;
+
+res = firstName + lastName;
+console.log(res);
+
+res = firstName + ' '+ lastName;
+console.log(res);
