0c62d3cb451b19e4f8afe5640b70deccf1d3f047
sunctsun <43174141+sunctsun@users.noreply.github.com>
2019-02-26 21:56:25 +0800

    Add files via upload

---
+++ 1N/w02-8-Dates_Times/app.js
@@ -0,0 +1,12 @@
+let val;
+
+name = '李昕庭';
+console.log(name);
+const today =new Date();
+console.log(today);
+let birthday = new Date('3-11-2000 20:03');
+console.log(birthday);
+birthday = new Date('March 11 2000');
+
+
+
---
+++ 1N/w02-9_If/app.js
@@ -0,0 +1,19 @@
+const man = 'man'
+
+const man_age = 25
+
+const woman = 'woman'
+const woman_age = 10
+
+if(man_age<=18){
+    console.log(`${man}is a kid`);
+}
+else if(man_age>18){
+    console.log(`${man}is a adult`);
+}
+if(woman_age<=18){
+    console.log(`${woman}is a kid`);
+}
+else if(woman_age>18){
+    console.log(`${woman}is a adult`);
+}
